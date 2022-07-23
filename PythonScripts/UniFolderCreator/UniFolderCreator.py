f48651de0c933e5bfc4c6e810e54280fc94f6133
R <@>
2022-07-23 17:43:35 +1000

    Switch to pathlib and further refactor

--- PythonScripts/UniFolderCreator/UniFolderCreator.py
+++ PythonScripts/UniFolderCreator/UniFolderCreator.py
@@ -1,33 +1,48 @@
 import os
 from sys import exit
+from pathlib import Path
 
-if __name__ == '__main__':
-    main()
+# Directory path to wherever you have your folder for the semester.
+# Eg 'C:\Users\Tim\Google Drive\2023 Sem 2'
+FOLDER_DIR: Path = Path.cwd()
+SEMESTER_WEEKS: int = 12 # Modify this if needed
+DRY_RUN: bool = True
 
-def main():
-    # Directory path to wherever you have your folder for the semester.
-    # Eg 'C:\Users\Tim\Google Drive\2023 Sem 2'
-    folder_dir = r"E:\QUT\2023 Semester 1"
-    semester_weeks = 15 # Modify this if needed
-
-    # Given a path to a folder, returns all folders within.
-    def get_subdirectories(directory):
-        return [f.name for f in os.scandir(folder_dir) if f.is_dir()]
-        
+def main() -> None:
     # Create a subfolder for each week of the semester for each subject folder.
-    subs = get_subdirectories(folder_dir)
-    if subs == []:
-        print("Create the main folders for your respective classes before running script!")
+    subs = get_subdirectories(FOLDER_DIR)
+
+    if len(subs) == 0:
+        print("ERROR:\tCreate the main folders for your respective classes before running script!")
         exit()
         
     for sub in subs:
-        for week in range(1, semester_weeks+1):
-            new_folder = f"Week {week}"
-            full_path = f"{folder_dir}\\{sub}\\{new_folder}"
-            print(f"Creating folder at {full_path}")
-            if os.path.exists(full_path):
-                print("Folder already exists!")
-                pass
+        make_directories(sub, SEMESTER_WEEKS)
+
+def get_subdirectories(directory: Path) -> list[Path]:
+    '''Returns each top-level subdirectory in a folder.'''
+    return [f for f in directory.iterdir() if f.is_dir()]
+
+def make_directories(parent: Path, weeks: int) -> None:
+    '''Creates a folder for each week in the specified parent.'''
+    for week in range(1, weeks+1):
+        new_folder_name = f"Week {week}"
+        full_path: Path = parent.joinpath(new_folder_name)
+
+        if full_path.exists():
+            print(f"WARNING:\tFolder {full_path} already exists!")
+            pass
+        else:
+            # print(f"INFO:\tFolder {full_path} being created...")
+            try:
+                if not DRY_RUN:
+                    full_path.mkdir(parents=True, exist_ok=True)
+                else:
+                    print(f"INFO: Dry run, pretending we created {full_path}...")
+            except Exception as e:
+                print(f"ERROR:\tFolder {full_path} could not be created. {str(e)}")
             else:
-                os.makedirs(full_path)
-                print(f"Folder successfully created at {full_path}")
+                print(f"INFO:\tFolder {full_path} successfully created.")
+
+if __name__ == '__main__':
+    main()
