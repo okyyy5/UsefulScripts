a57c1db0cb2e1d40028b54edb2558932e9c59c43
Brook <jeynesbrook@gmail.com>
2022-07-24 21:12:02 +1000

    program no longer crashes when a non-existing directory is input; added ability to specify units

--- PythonScripts/UniFolderCreator/UniFolderCreator.py
+++ PythonScripts/UniFolderCreator/UniFolderCreator.py
@@ -10,9 +10,44 @@ def main() -> None:
     # Directory path to wherever you have your folder for the semester.
     # Eg 'C:\Users\Tim\Google Drive\2023 Sem 2'
     try:
-        folder_dir: Path = Path(sys.argv[1])
-    except Exception:
+        folder_dir: Path = Path(get_arg(1))
+        sub_folders: list = get_arg(2).split()
+
+        # Check if chosen DIR doesn't exist
+        if not folder_dir.exists():
+            print(f"ERROR:\tFolder {folder_dir} does not exist! Would you like to create it?")
+
+            answer = input("(y/n): ")
+
+            while answer not in ["y", "n"]:
+                print("Please enter 'y' or 'n'.")
+                answer = input("(y/n): ")
+
+            if answer == "n":
+                print("Please restart the application to enter a new path")
+                exit()
+            elif answer == "y":
+                try:
+                    os.makedirs(folder_dir)
+                except Exception as e:
+                    print(f"ERROR:\tFolder {folder_dir} could not be created. {str(e)}")
+                    exit()
+                else:
+                    print(f"INFO:\tFolder {folder_dir} successfully created.")
+
+        # Create a subdirectory for each specified unit
+        if len(sub_folders) > 0:
+            for folder in sub_folders:
+                try:
+                    os.makedirs(f'{folder_dir}/{folder}')
+                except Exception as e:
+                    print(f"ERROR:\tFolder {folder} could not be created. {str(e)}")
+                else:
+                    print(f"INFO:\tFolder {folder} successfully created.")
+                    
+    except Exception as e:
         print("ERROR:\tThe entered path is not valid")
+        print(e)
         exit()
 
     # Create a subfolder for each week of the semester for each subject folder.
@@ -49,6 +84,15 @@ def make_directories(parent: Path, weeks: int) -> None:
                 print(f"ERROR:\tFolder {full_path} could not be created. {str(e)}")
             else:
                 print(f"INFO:\tFolder {full_path} successfully created.")
+
+def get_arg(index):
+    try:
+        sys.argv[index]
+    except IndexError:
+        return ''
+    else:
+        return sys.argv[index]
+
 
 if __name__ == '__main__':
     main()
